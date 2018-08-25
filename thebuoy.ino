339990408ef705277b56370e567e7a09ed7dfd12
Evan Magoni <esm367@gmail.com>
2018-08-25 17:55:01 -0400

    New: button switches LED color

--- thebuoy.ino
+++ thebuoy.ino
@@ -1,24 +1,31 @@
 /*
-  Modified from: http://www.arduino.cc/en/Tutorial/Tone2
-  originally by Tom Igoe, with suggestion from Michael Flynn
-
-  Modified 23 Aug 2018
+  Modified from: http://www.arduino.cc/en/Tutorial/Tone2 and https://www.arduino.cc/en/Tutorial/Debounce
   by Evan Magoni
 */
 
 /* Configuration */
 const int buttonPin = 8;
-const int ledPin = 10;
+const int ledPins[] = {10, 9};
 const int audioPin = 6;
 const int sensorMax = 700;
 const int sensorMin = 200;
 int audioThreshold = 720;
 /* End Configuration */
 
-int buttonState = 0;
+/* Global State */
+int ledState = 0; /* 0 or 1 = pin 10 or 9*/
+int buttonState;
 int lastButtonState = 0;
+int ledPin = ledPins[0];
+int secondaryLedPin = ledPins[1];
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+/* End Global State */
 
 void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(audioPin, OUTPUT);
   // initialize serial communications (for debugging only):
   Serial.begin(9600);
 }
@@ -28,13 +35,51 @@ void loop() {
   // TODO read ultrasonic sensor on modeswitch https://www.sunfounder.com/ultrasonic-module-hc-sr04-distance-sensor.html
   // TODO copy sensorValue to audioThreshold when mode switches to photoresistor
   
+  int buttonValue = digitalRead(buttonPin);
   int photoResistorValue = analogRead(A0);
+  handleMode(buttonValue);
   generateAudio(photoResistorValue);
   delay(1);        // delay in between reads for stability
 }
 
+void handleMode(int buttonValue) {
+  if (buttonValue != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the buttonValue is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (buttonValue != buttonState) {
+      buttonState = buttonValue;
+
+      // only toggle the LED if the new button state is ON (LOW)
+      if (buttonState == LOW) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  if (ledState) {
+    // output on pin 10
+    ledPin = ledPins[0];
+    secondaryLedPin = ledPins[1];
+    analogWrite(secondaryLedPin, 255); // turn off the other pin
+  } else {
+    ledPin = ledPins[1];
+    secondaryLedPin = ledPins[0];
+    analogWrite(secondaryLedPin, 255); // turn off the other pin
+  }
+  
+  // save the buttonValue. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = buttonValue;
+}
+
 void generateAudio(int sensorValue) {
-  // print the sensor reading so you know its range
   Serial.println(sensorValue);
 
   // TODO read maxpitch from a knob
